9ded5ecee3c26b8186ac69f0bffc8536b62fc0f5
André Batista <hi@andrebatista.dev>
2019-07-19 18:13:37 +0100

    add nextjs "_app" and "_document" files

---
+++ pages/_app.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+
+import theme from 'app/theme';
+
+class MyApp extends App {
+  public render(): JSX.Element {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Container>
+    );
+  }
+}
+
+MyApp.getInitialProps = async ({ Component, ctx }: any): Promise<any> => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps };
+};
+
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+import { ServerStyleSheets } from '@material-ui/styles';
+
+class MyDocument extends Document {
+  public render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx: any): Promise<any> => {
+  const sheets = new ServerStyleSheets();
+  const { renderPage } = ctx;
+
+  ctx.renderPage = (): any =>
+    renderPage({
+      enhanceApp: (App: any): any => (props: any): any => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        {sheets.getStyleElement()}
+      </>
+    ),
+  };
+};
+
+export default MyDocument;
