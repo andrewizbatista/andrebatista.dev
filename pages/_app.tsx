eec62e06896dbe5516f5b8b6624244297b30a1b0
André Batista <hi@andrebatista.dev>
2019-07-28 22:13:44 +0100

    add react strict mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import App, { Container } from 'next/app';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -7,18 +7,20 @@ import { ThemeProvider } from '@material-ui/styles';
 import theme, { GlobalStyles } from 'app/theme';
 
 class MyApp extends App {
-  public render(): JSX.Element {
+  public render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <GlobalStyles>
-        <ThemeProvider theme={theme}>
-          <Container>
-            <CssBaseline />
-            <Component {...pageProps} />
-          </Container>
-        </ThemeProvider>
-      </GlobalStyles>
+      <StrictMode>
+        <GlobalStyles>
+          <ThemeProvider theme={theme}>
+            <Container>
+              <CssBaseline />
+              <Component {...pageProps} />
+            </Container>
+          </ThemeProvider>
+        </GlobalStyles>
+      </StrictMode>
     );
   }
 }
