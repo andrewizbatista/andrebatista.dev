593a4854604ff79822ff59009d8438d6d2e4d0b0
André Batista <hi@andrebatista.dev>
2019-08-02 15:11:01 +0100

    add polyfills to support IE11

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   webpack: (config) => {
+    const entryOriginal = config.entry;
+
+    config.entry = async () => {
+      const entries = await entryOriginal();
+
+      const mainJS = entries['main.js'];
+      const polyfills = './app/polyfills.ts';
+
+      if (mainJS && !mainJS.includes(polyfills)) {
+        entries['main.js'].unshift(polyfills);
+      }
+
+      return entries;
+    };
+
     config.resolve.modules.push('.'); // resolve root imports
 
     return config;
